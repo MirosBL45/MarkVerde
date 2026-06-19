4dbab72cd5cdf5e8644039d16910d8cbad7b043f
MirosBL45 <jovicmiroslav88@gmail.com>
2026-06-19 17:08:04 +0200

    added images, type, blog data

---
+++ types/blog.ts
@@ -0,0 +1,10 @@
+export interface BlogPost {
+  slug: string;
+  title: string;
+  excerpt: string;
+  author: string;
+  date: string;
+  readingTime: string;
+  tags: string[];
+  content: string;
+}
